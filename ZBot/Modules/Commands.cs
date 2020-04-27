14d566da5150dc00dbe9c1e5593ee8d8777431ae
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2020-04-27 16:59:50 +0200

    ..

--- ZBot/Modules/Commands.cs
+++ ZBot/Modules/Commands.cs
@@ -58,7 +58,7 @@ namespace ZBot.Modules
         [Summary("nothing")]
         public async Task Hack()
         {
-            await ReplyAsync("IM BEING HACKED.eeek");
+            await ReplyAsync("IM BEING HACKED.maybe");
         }
 
         [Command("source")]
