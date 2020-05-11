7a76872ed91dda042e8c9c084d9edb1007a680be
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2020-05-11 20:26:41 +0200

    Changed functions to not be generic

--- ZBot/Services/RiotApiRequests.cs
+++ ZBot/Services/RiotApiRequests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Zbot.Models;
 using ZBot.Modules;
 
 namespace ZBot
@@ -14,7 +15,7 @@ namespace ZBot
             _apiHandler = apiHandler;
         }
 
-        public async Task<RiotApiResponseSummoner> GetSummoner<RiotApiResponseSummoner>(string summonerName)
+        public async Task<RiotApiResponseSummoner> GetSummoner(string summonerName)
         {
             var escapedName = Uri.EscapeUriString(summonerName); //Need to escape incase it has a space in the name
             string urlSummoner = $"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{escapedName}"; //Url to get summoner info with summonername
@@ -22,20 +23,20 @@ namespace ZBot
             return await _apiHandler.ApiRequest<RiotApiResponseSummoner>(urlSummoner);
         }
 
-        public async Task<T> GetSummonerRank<T>(string summonerName)
+        public async Task<RiotApiResponseRank[]> GetSummonerRank(string summonerName)
         {
-            var summoner = await GetSummoner<RiotApiResponseSummoner>(summonerName);
+            var summoner = await GetSummoner(summonerName);
             string urlRank = $"https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner.Id}"; //Url to get ranked information of summoner. It only accepts summoner id
 
-            return await _apiHandler.ApiRequest<T>(urlRank);
+            return await _apiHandler.ApiRequest<RiotApiResponseRank[]>(urlRank);
         }
 
-        public async Task<T> GetMatch<T>(string summonerName)
+        public async Task<LeagueMatch> GetMatch(string summonerName)
         {
-            var summoner = await GetSummoner<RiotApiResponseSummoner>(summonerName);
+            var summoner = await GetSummoner(summonerName);
             string urlRank = $"https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner.Id}"; //Url to get match. It only accepts summoner id
 
-            return await _apiHandler.ApiRequest<T>(urlRank);
+            return await _apiHandler.ApiRequest<LeagueMatch>(urlRank);
         }
     }
 }
