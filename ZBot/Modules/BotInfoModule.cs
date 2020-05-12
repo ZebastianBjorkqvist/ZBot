039f3db4157b69860f5212291148a30cf7bebc28
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2020-05-12 19:44:05 +0200

    Moved !uptime and !source to new class.

---
+++ ZBot/Modules/BotInfoModule.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBot.Modules
+{
+    public class BotInfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("source")]
+        [Summary("Posts GitHub Link")]
+        public async Task Source() => 
+            await ReplyAsync("https://github.com/ZebastianBjorkqvist/ZBot/");
+
+        [Command("uptime")]
+        [Summary("Displays bot uptime")]
+        public async Task Uptime()
+        {
+            DateTime startup = Process.GetCurrentProcess().StartTime;
+            TimeSpan uptime = DateTime.Now - startup;
+
+            var days = uptime.Days + " day" + (uptime.Days != 1 ? "s" : "");
+            var hours = uptime.Hours + " hour" + (uptime.Hours != 1 ? "s" : "");
+            var mins = uptime.Minutes + " min" + (uptime.Minutes != 1 ? "s" : "");
+
+            await ReplyAsync($"The bot has been up for {days}, {hours} and {mins}");
+        }
+    }
+}
--- ZBot/Modules/CommandsModule.cs
+++ ZBot/Modules/CommandsModule.cs
@@ -60,30 +60,5 @@ namespace ZBot.Modules
             }
             await ReplyAsync($"no! {user.Username} {user.Mention}");
         }
-
-        [Command("hack")]
-        [Summary("nothing")]
-        public async Task Hack()
-        {
-            await ReplyAsync("IM BEING HACKED.Hello there");
-        }
-
-        [Command("source")]
-        [Summary("Posts GitHub Link")]
-        public async Task Source() => await ReplyAsync("https://github.com/ZebastianBjorkqvist/ZBot/");
-
-        [Command("uptime")]
-        [Summary("Displays bot uptime")]
-        public async Task Uptime()
-        {
-            DateTime startup = Process.GetCurrentProcess().StartTime;
-            TimeSpan uptime = DateTime.Now - startup;
-
-            var days = uptime.Days + " day" + (uptime.Days != 1 ? "s" : "");
-            var hours = uptime.Hours + " hour" + (uptime.Hours != 1 ? "s" : "");
-            var mins = uptime.Minutes + " min" + (uptime.Minutes != 1 ? "s" : "");
-            
-            await ReplyAsync($"The bot has been up for {days}, {hours} and {mins}");
-        }
     }
 }
